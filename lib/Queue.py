01071a5e3a5e3b1807f4a9901eb078b17ba9ff49
relikd <info@relikd.de>
2017-12-24 19:13:50 +0100

    queue prioritizing

--- lib/Queue.py
+++ lib/Queue.py
@@ -4,32 +4,37 @@ import heapq
 
 
 class QQueue(object):
-    def __init__(self):
+    def __init__(self, constraintList):
         super(QQueue, self).__init__()
         self.stack = []
         heapq.heapify(self.stack)
+        self.cs = constraintList
         self.previousItem = None
-
-    def init(self):
-        self.previousItem = heapq.heappop(self.stack)
 
     def dequeue(self):
         var = heapq.heappop(self.stack)
         while var == self.previousItem and len(self.stack) > 0:
             var = heapq.heappop(self.stack)
-        # return var[1]
-        retVal = self.previousItem[1]
-        self.previousItem = var
-        return retVal
+        # print "get", var
+        return var[1]
+
+    def getPriority(self, element):
+        constraints = self.cs[element[0]][element[1]]
+        # TODO: implement dictionary lookup for priority
+        return constraints
+
+    def enqueue(self, element):
+        prio = self.getPriority(element)
+        var = (prio, element)
+        # print " -> put", var
+        heapq.heappush(self.stack, var)
 
     def enqueueNew(self, element):
-        var = (element[1] % 4, element)
-        # for x in self.stack:
-        #     if x[1] == var[1]:
-        #         return
-        # if var in self.stack:
-        #     return
-        heapq.heappush(self.stack, var)
+        for x in range(len(self.stack)):
+            if self.stack[x][1] == element:
+                del self.stack[x]  # cause new priority most likely differ
+                break
+        self.enqueue(element)
 
     def isEmpty(self):
         return len(self.stack) == 0
